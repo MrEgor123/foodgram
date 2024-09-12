870e91b3a504ce18d3a2c8d657703a26145c0913
Егор Чарушин <egor.charus2402@mail.ru>
2024-09-12 08:55:20 +0300

    Processing

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -16,7 +16,6 @@ class Ingredient(models.Model):
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
         ordering = ['name']
-        ordering = ['name']
 
     def __str__(self):
         return f'{self.name}, {self.measurement_unit}'
